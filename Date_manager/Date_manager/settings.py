e7698974c839585f11aa4fba9317c617bfa9b884
RHX <mercerfuran@gmail.com>
2020-05-11 13:04:46 +0800

    添加了首页，打算添加登录及注册功能

--- Date_manager/Date_manager/settings.py
+++ Date_manager/Date_manager/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'Today'  # 注册today
+    'Today',  # 注册today
+    'Login',  # 注册login
 ]
 
 MIDDLEWARE_CLASSES = [
