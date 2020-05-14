16ee2c50998079fb01ee2d44e4a55dafc7fa2a31
RHX <mercerfuran@gmail.com>
2020-05-14 19:17:33 +0800

    优化登录及注册

--- Date_manager/Login/urls.py
+++ Date_manager/Login/urls.py
@@ -4,4 +4,6 @@ from Login import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^login/$', views.login),
+    url(r'^login_check/$', views.login_check),
+    url(r'^register/$', views.register)
 ]
--- Date_manager/Login/views.py
+++ Date_manager/Login/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from Login.models import User
+import sys
 
 
 # Create your views here.
@@ -11,11 +13,31 @@ def index(request):
 
 def login(request):
     """显示登录界面"""
-    if request != None:
+    if request.method == 'POST':
         u = User()
         u.user_name = request.POST.get('username')
         u.user_password = request.POST.get('password')
         print(u.user_name, u.user_password)
         return redirect("/today/")
     else:
-        return "error"
+        return render(request, 'index/login.html')
+
+
+def login_check(request):
+    """检查登录"""
+    if request.method == "POST":
+        username = request.POST.get("input_username")
+        password = request.POST.get("input_pwd")
+        try:
+            u = User.objects.get(user_name=username)
+        except:
+            print(sys.exc_info())  # 打印异常信息
+            return JsonResponse({'res': '0'})  # 出现异常则失败返回0
+        else:
+            return JsonResponse({'res': '1'})  # 未出现异常则返回1
+
+
+def register(request):
+    if request.method == 'POST':
+        return JsonResponse({})
+    return render(request, 'index/register.html')
