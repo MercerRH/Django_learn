d64340ab1ce91aafd78d5980d78ebf9458c94d37
RHX <mercerfuran@gmail.com>
2020-05-16 20:23:44 +0800

    优化

--- Date_manager/Today/models.py
+++ Date_manager/Today/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from Login.models import User
 # Create your models here.
 
 
 class Event(models.Model):
+    """事件模型类"""
+    user_id = models.ForeignKey('Login.User')
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=500)
     start_time = models.DateTimeField()
